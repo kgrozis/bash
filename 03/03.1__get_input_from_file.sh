042c91ae1aa51ba67b2d6a4063bd1c708193ed05
kevingrozis@juniper.net <kevingrozis@juniper.net>
2018-07-11 15:23:31 -0400

    created 03 dir and added 3.1, 3.2, and ext files

---
+++ 03/03.1__get_input_from_file.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+# want shell to read input from a file
+# use input redirection < character to read data
+wc my.file
---
+++ 03/03.2__get_input_from_cmdline.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# here-documnent << chars
+#   redirects text from the command line 
+
+# view ext file
+# paramter sent to ext is substituted in grep statement
+bash ext bill
+bash ext x.555
+
+# could add -i to grep statement in ext to make it case sensitive.
